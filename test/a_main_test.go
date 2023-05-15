fec7cabeb4acc4d311c1300f91f1aab7a0f935ea
Allan Nava <allannava95@gmail.com>
2023-05-15 12:13:49 +0200

    added a main test

---
+++ test/a_main_test.go
@@ -0,0 +1,29 @@
+package test 
+
+
+import (
+	"os"
+	"testing"
+)
+
+
+func TestMain(m *testing.M) {
+	if os.Getenv("APP_ENV") == "" {
+		err := os.Setenv("APP_ENV", "test")
+		if err != nil {
+			panic("could not set test env")
+		}
+	}
+	env.Load()
+	m.Run()
+}
+
+func GetGoBard() gobard.IGoBard {
+	//
+	configuration := configuration.GetConfiguration()
+	g := gobard.NewGoBoard(
+		configuration
+	)
+	//
+	return g
+}
