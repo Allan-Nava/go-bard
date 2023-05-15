e8b5e3302ab54a115be561853359f823e684dac8
Allan Nava <allannava95@gmail.com>
2023-05-15 12:01:06 +0200

    fix

--- gobard/gobard.go
+++ gobard/gobard.go
@@ -13,14 +13,14 @@ type gobard struct {
 	restClient    *resty.Client
 }
 
-func NewFakeYou(configuration *configuration.Configuration) IGoBard {
-	fk := &gobard{
+func NewGoBoard(configuration *configuration.Configuration) IGoBard {
+	g := &gobard{
 		configuration: configuration,
 	}
-	fk.restClient = resty.New()
-	//fk.restClient.SetBaseURL(routes.BASE_URL)
+	g.restClient = resty.New()
+	//g.restClient.SetBaseURL(routes.BASE_URL)
 	/*
-		fk.restClient.SetHeader("Referer", "https://bard.google.com/")
+		g.restClient.SetHeader("Referer", "https://bard.google.com/")
 		//"Host": "bard.google.com",
 		//"X-Same-Domain": "1",
 		//"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
@@ -28,23 +28,23 @@ func NewFakeYou(configuration *configuration.Configuration) IGoBard {
 		//"Origin": "https://bard.google.com",
 		//"Referer": "https://bard.google.com/",
 	*/
-	fk.restClient.SetHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
-	fk.restClient.SetHeader("Host", "bard.google.com")
-	fk.restClient.SetHeader("X-Same-Domain", "1")
-	fk.restClient.SetHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36")
-	fk.restClient.SetHeader("Origin", "https://bard.google.com")
+	g.restClient.SetHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+	g.restClient.SetHeader("Host", "bard.google.com")
+	g.restClient.SetHeader("X-Same-Domain", "1")
+	g.restClient.SetHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36")
+	g.restClient.SetHeader("Origin", "https://bard.google.com")
 	if configuration.IsDebug {
-		fk.restClient.SetDebug(true)
+		g.restClient.SetDebug(true)
 	}
-	return fk
+	return g
 }
 
 
 
 // Resty Methods
 
-func (f *fakeyou) restyPost(url string, body interface{}) (*resty.Response, error) {
-	resp, err := f.restClient.R().
+func (o *goboard) restyPost(url string, body interface{}) (*resty.Response, error) {
+	resp, err := o.restClient.R().
 		SetHeader("Accept", "application/json").
 		SetBody(body).
 		Post(url)
@@ -55,7 +55,7 @@ func (f *fakeyou) restyPost(url string, body interface{}) (*resty.Response, erro
 	return resp, nil
 }
 
-func (o *fakeyou) restyGet(url string, queryParams map[string]string) (*resty.Response, error) {
+func (o *goboard) restyGet(url string, queryParams map[string]string) (*resty.Response, error) {
 	resp, err := o.restClient.R().
 		SetQueryParams(queryParams).
 		Get(url)
