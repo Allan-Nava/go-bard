f03428dc1fbbe2f0566b402170aac973c91d487d
Allan Nava <allannava95@gmail.com>
2023-05-15 12:00:04 +0200

    added resty methods

--- gobard/gobard.go
+++ gobard/gobard.go
@@ -38,3 +38,30 @@ func NewFakeYou(configuration *configuration.Configuration) IGoBard {
 	}
 	return fk
 }
+
+
+
+// Resty Methods
+
+func (f *fakeyou) restyPost(url string, body interface{}) (*resty.Response, error) {
+	resp, err := f.restClient.R().
+		SetHeader("Accept", "application/json").
+		SetBody(body).
+		Post(url)
+
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (o *fakeyou) restyGet(url string, queryParams map[string]string) (*resty.Response, error) {
+	resp, err := o.restClient.R().
+		SetQueryParams(queryParams).
+		Get(url)
+	//
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
